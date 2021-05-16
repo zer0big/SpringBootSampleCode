34ef36bb5551fd1902fc03adee03999966c6a6b9
zerobig <zerobig.kim@gmail.com>
2021-05-16 12:17:50 +0900

    first commit

---
+++ src/test/java/com/zerobig/springboothandson/SpringbootHandsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobig.springboothandson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHandsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
